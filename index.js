146b121f72cad31bec161669f0b994f78499760b
brian51665 <bdavis51892@yahoo.com>
2017-10-04 20:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
     var itemNameAndPrice = cart[i];
     var itemName = Object.keys(itemNameAndPrice);
     var itemPrice = itemNameAndPrice[itemName];
-    console.log(`In your cart, you have ${itemName.join("and ")} at ${itemPrice.join(", ")}.`);
+    console.log(In your cart, you have);
     i++;
   }
 }
