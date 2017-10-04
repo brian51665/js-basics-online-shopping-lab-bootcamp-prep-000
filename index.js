655ca67639e0064690fcd7424894c18129723dcd
brian51665 <bdavis51892@yahoo.com>
2017-10-04 18:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  var itemName = `${item}`;
   var itemPrice = Math.floor(Math.random() * 101);
-  var itemInfo = {item : itemPrice};
+  var itemInfo = {itemName : itemPrice};
  cart.push(itemInfo);
  console.log(`"${item} has been added to your cart."`)
 }
