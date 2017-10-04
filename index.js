4f0284ee38ac3ed61417c9536e5209c5825dcaa1
brian51665 <bdavis51892@yahoo.com>
2017-10-04 20:15:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,14 +21,6 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.")
-  } else if (cart.length === 1){
-    var i = 0;
-    for (let i = 0; i < cart.length; i++);
-    var itemNameAndPrice = cart[i];
-    var itemName = Object.keys(itemNameAndPrice);
-    var itemPrice = itemNameAndPrice[itemName];
-    console.log("In your cart, you have " + itemName + " at $" + itemPrice + ".");
-    i++;
   } else {
     var i = 0;
     for (let i = 0; i < cart.length; i++);
@@ -37,7 +29,7 @@ function viewCart() {
     var itemPrice = itemNameAndPrice[itemName];
     console.log("In your cart, you have " + itemName + " at $" + itemPrice + ".");
     i++;
-  }
+  } 
 }
 
 /*
