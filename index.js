1a2d42faab4db6841dc80b2080b24a548d120451
brian51665 <bdavis51892@yahoo.com>
2017-10-04 18:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemName = `${item}`;
+  var itemName = item;
   var itemPrice = Math.floor(Math.random() * 101);
   var itemInfo = {itemName : itemPrice};
  cart.push(itemInfo);
