c5f8c9a3228b1fa5febf9a8e2711f94833a61528
brian51665 <bdavis51892@yahoo.com>
2017-10-04 19:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
     var itemNameAndPrice = cart[i];
     var itemName = Object.keys(itemNameAndPrice);
     var itemPrice = Object.values(itemNameAndPrice);
-    console.log("In your cart, you have " + Object.keys(cart[i]) + " at $" + Object.values(cart[i])+ ".");
+    console.log("In your cart, you have " + itemName + " at $" + itemPrice + ".");
     i++;
   }
 }
